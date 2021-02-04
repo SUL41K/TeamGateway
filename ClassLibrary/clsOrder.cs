70fb889b5ad9562136f4a7ad3386416fcaca56f6
77masterdawid77 <P2516411@my365.mydmu.ac.uk>
2021-02-04 16:57:17 +0000

    added 4 more tests to Order

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,27 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public string GameTitle { get; set; }
+
+        public string Valid(string gGameTitle)
+        {
+            if (gGameTitle.Length < 1)
+            {
+                return "description cannot be blank";
+            }
+
+            if (gGameTitle.Length > 50)
+            {
+                return "description cannot be more then 50 characters";
+            }
+
+            else
+            {
+                return "";
+            }
+        }
     }
 }
--- Testing3/tstOrder.cs
+++ Testing3/tstOrder.cs
@@ -7,11 +7,55 @@ namespace Testing3
     [TestClass]
     public class tstOrder
     {
+        //good test data
+        private string gGameTitle = "Some Game Title";
+
         [TestMethod]
         public void InstanceOk()
         {
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
         }
+
+        [TestMethod]
+        public void GameTitleOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = gGameTitle;
+            AnOrder.GameTitle = TestData;
+            Assert.AreEqual(AnOrder.GameTitle, TestData);
+        }
+
+        [TestMethod]
+        public void ValidOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string Error = "";
+            string TestData = gGameTitle;
+            Error = AnOrder.Valid(gGameTitle);
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void MinLessOne()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string Error = "";
+            string TestData = "";
+            Error = AnOrder.Valid(TestData);
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void MaxPlusOne()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string Error = "";
+            string TestData = "";
+            TestData = TestData.PadLeft(51, '*');
+            Error = AnOrder.Valid(TestData);
+            Assert.AreNotEqual(Error, "");
+        }
+
     }
 }
