0f3bf80eb84a7b1eff7e2849d464942c54286178
SUL41K <sulaiknoor@hotmail.co.uk>
2021-02-06 19:22:19 +0000

    Added all tests for the stock class and all have passed

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,32 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public int gameID { get; set; }
+        public string gameName { get; set; }
+        public bool Availability { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Price { get; set; }
+        public int AgeRating { get; set; }
+
+        public string Valid(string gGameName)
+        {
+            if (gGameName.Length < 1)
+            {
+                return "description cannot be blank";
+            }
+
+            if (gGameName.Length > 50)
+            {
+                return "description cannot be more then 50 characters";
+            }
+
+            else
+            {
+                return "";
+            }
+        }
     }
 }
