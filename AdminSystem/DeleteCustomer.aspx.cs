6f58695b96554e4225e7a69c4da6564d6d378392
Hamza2210 <hamzaash2001@gmail.com>
2021-03-18 14:43:37 +0000

    Added comments to all code

--- AdminSystem/DeleteCustomer.aspx.cs
+++ AdminSystem/DeleteCustomer.aspx.cs
@@ -8,23 +8,28 @@ using ClassLibrary;
 
 public partial class DeleteCustomer : System.Web.UI.Page
 {
-
+    //customerID variable at page scope to ensure the variable applies to entire page
     Int32 CustomerId;
     protected void Page_Load(object sender, EventArgs e)
     {
+        //customerId is converted to Int and applied to customerId session (database)
         CustomerId = Convert.ToInt32(Session["customerId"]);
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
+        //create new instance of customer collection class
         clsCustomerCollection CustomerBook = new clsCustomerCollection();
         CustomerBook.ThisCustomer.Find(CustomerId);
+        //invoke the delete method
         CustomerBook.Delete();
+        //redirect to customer list once confirmed
         Response.Redirect("CustomerList.aspx");
     }
 
     protected void btnNo_Click(object sender, EventArgs e)
     {
+        //redirect to customer list on click
         Response.Redirect("CustomerList.aspx");
     }
 }
--- ClassLibrary/clsCustomerCollection.cs
+++ ClassLibrary/clsCustomerCollection.cs
@@ -11,10 +11,14 @@ namespace ClassLibrary
             Int32 Index = 0;
             Int32 RecordCount = 0;
 
+            //create new instance of data connection class
             clsDataConnection DB = new clsDataConnection();
+            //excecute select all stored procedure
             DB.Execute("sproc_tblCustomer_SelectAll");
             PopulateArray(DB);
 
+            //creates new instance of clsCustomer
+            //Adds all data entered in each field to create a new customer. Adds customer and increments index of each attribute
             while (Index < RecordCount)
             {
                 clsCustomer ACustomer = new clsCustomer();
@@ -70,40 +74,47 @@ namespace ClassLibrary
 
         public int Add()
         {
+            //create new instance of data connection class
             clsDataConnection DB = new clsDataConnection();
+            //add parameters to table fields to create a new customer
             DB.AddParameter("@customerName", mThisCustomer.CustomerName);
             DB.AddParameter("@customerEmail", mThisCustomer.CustomerEmail);
             DB.AddParameter("@customerDOB", mThisCustomer.CustomerDOB);
             DB.AddParameter("@customerSubscribe", mThisCustomer.CustomerSubscribe);
-
+            //execute stored procedure which inserts new data into the table
             return DB.Execute("sproc_tblCustomer_Insert");
 
         }
 
         public void Update()
         {
+            //create new instance of data connection class
             clsDataConnection DB = new clsDataConnection();
-
+            //add parameters to table fields to create new details for customer
             DB.AddParameter("@customerId", mThisCustomer.CustomerId);
             DB.AddParameter("@customerName", mThisCustomer.CustomerName);
             DB.AddParameter("@customerEmail", mThisCustomer.CustomerEmail);
             DB.AddParameter("@customerDOB", mThisCustomer.CustomerDOB);
             DB.AddParameter("@customerSubscribe", mThisCustomer.CustomerSubscribe);
-
+            //execute stored procedure which updates existing customer data with newly inputted data
             DB.Execute("sproc_tblCustomer_Update");
         }
 
         public void Delete()
         {
+            //create new instance of data connection class
             clsDataConnection DB = new clsDataConnection();
             DB.AddParameter("customerId", mThisCustomer.CustomerId);
+            //execute stored procedure which deletes primary key record
             DB.Execute("sproc_tblCustomer_Delete");
         }
 
         public void ReportByName(string Name)
         {
+            //create new instance of data connection class
             clsDataConnection DB = new clsDataConnection();
             DB.AddParameter("@customerName", Name);
+            //execute stored procedure which filters records by first name
             DB.Execute("sproc_tblCustomer_FilterBycustomerName");
             PopulateArray(DB);
         }
