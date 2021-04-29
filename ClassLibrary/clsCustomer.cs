59d2c6a1838e5e53303a1193fcfab6e0de8fbb64
Hamza2210 <hamzaash2001@gmail.com>
2021-04-29 14:11:27 +0100

    Minor fixes and comments

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -4,12 +4,14 @@ namespace ClassLibrary
 {
     public class clsCustomer
     {
+        //private declarations of each attribute
         private string mCustomerName;
         private Int32 mCustomerId;
         private DateTime mCustomerDOB;
         private string mCustomerEmail;
         private Boolean mCustomerSubscribe;
 
+        //get and set method to return customer name
         public string CustomerName
         {
             get
@@ -22,6 +24,8 @@ namespace ClassLibrary
                 mCustomerName = value;
             }
         }
+
+        //get and set method to return customer Id
         public Int32 CustomerId
         {
             get
@@ -34,6 +38,7 @@ namespace ClassLibrary
             }
         }
 
+        //get and set method to return customer DOB
         public DateTime CustomerDOB
         {
             get
@@ -46,57 +51,84 @@ namespace ClassLibrary
             }
         }
 
+        //Validation method to ensure the correct and accurate data is being inputted for each attribute
         public string Valid(string customerName, string customerEmail, string customerDOB)
         {
+            //blank string for error message
             String Error = "";
 
+            //date time variables added to create age range for DOB
             DateTime DateTemp;
             DateTime Date1950;
             DateTime DateMax;
+
+            //Setting condition if name is empty
             if (customerName.Length == 0)
             {
+                //Update error message string if field is empty
                 Error = Error + "The Customer Name may not be blank : ";
             }
 
+            //Setting condition if name is over 30 characters
             if (customerName.Length > 30)
             {
+                //Update error message if field is over 30 characters
                 Error = Error + "The Customer Name must be less than 30 characters : ";
             }
 
+            //Setting condition if email is empty
             if (customerEmail.Length == 0)
             {
+                //Update error message string if field is empty
                 Error = Error + "The Customer email may not be blank : ";
             }
 
+            //Setting condition if email is over 50 characters
             if (customerEmail.Length > 50)
             {
+                //Update error message if email is over 50 characters
                 Error = Error + "The Customer email must be less than 50 characters : ";
+            }
+
+            if (String.IsNullOrEmpty(customerName))
+            {
+                Error = Error + "The name must not contain numbers";
             }
 
             try
             {
+                //Making variable value the same as the customer DOB
                 DateTemp = Convert.ToDateTime(customerDOB);
+                //Setting minimum valid date
                 Date1950 = new DateTime(1950, 01, 01);
+                //setting maximum valid date
                 DateMax = new DateTime(2010, 01, 01);
+
+                //Setting condition if DOB is earlier than minimum valid date
                 if (DateTemp < Date1950)
                 {
+                    //Updates error message
                     Error = Error + "The date cannot be before 1950 : ";
                 }
 
+                //Setting condition if date is later than maximum date set
                 if (DateTemp > Date1950 && DateTemp > DateMax)
                 {
-                    Error = Error + "Please enter a valid DOB : ";
+                    //Updates error message
+                    Error = Error + "The date cannot be after 2010 : ";
                 }
 
             }
             catch
             {
+                //Updates error message if incorrect data was inputted
                 Error = Error + "The date was not a valid date : ";
             }
 
             return Error;
         }
 
+        //set and get method to return email
         public string CustomerEmail
         {
             get
@@ -108,7 +140,8 @@ namespace ClassLibrary
                 mCustomerEmail = value;
             }
         }
-                  
+
+        //set and get method to return boolean checkbox value
         public Boolean CustomerSubscribe
         {
             get
@@ -147,12 +180,13 @@ namespace ClassLibrary
             if (DB.Count == 1)
             {
 
-               
+
                 mCustomerId = Convert.ToInt32(DB.DataTable.Rows[0]["customerId"]);
                 mCustomerDOB = Convert.ToDateTime(DB.DataTable.Rows[0]["customerDOB"]);
                 mCustomerName = Convert.ToString(DB.DataTable.Rows[0]["customerName"]);
                 mCustomerEmail = Convert.ToString(DB.DataTable.Rows[0]["customerEmail"]);
                 mCustomerSubscribe = Convert.ToBoolean(DB.DataTable.Rows[0]["customerSubscribe"]);
+                //returns all of the found data attributes into each field
                 return true;
             }
 
